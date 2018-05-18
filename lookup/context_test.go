02e8c86b703f5eea2b73402915b2abb2b3ae4240
Thomas Hallgren <thomas@tada.se>
2018-05-18 07:01:35 +0200

    Added TypeSet describing the Hiera config

--- lookup/context_test.go
+++ lookup/context_test.go
@@ -11,26 +11,47 @@ import (
 	_ "github.com/puppetlabs/go-hiera/functions"
 )
 
+var sampleData = map[string]eval.PValue {
+	`first`: eval.Wrap(`value of first`),
+	`arr`: eval.Wrap([]string{`one`, `two`, `three`}),
+	`hash`: eval.Wrap(map[string]interface{}{`arr`: []string{`value 1`, `value 2`}}),
+	`second`: eval.Wrap(`includes %lookup('first')'`),
+}
+
+func sampleProvider(c eval.Context, key string, options eval.KeyedValue) (eval.PValue, bool, error) {
+	v, ok := sampleData[key]
+	return v, ok, nil
+}
+
 func ExampleDoWithParent() {
-	sampleData := map[string]eval.PValue {
-		`first`: eval.Wrap(`value of first`),
-		`arr`: eval.Wrap([]string{`one`, `two`, `three`}),
-		`second`: eval.Wrap(`includes %lookup('first')'`),
-	}
-
-	provider := func(c eval.Context, key string, options eval.KeyedValue) (eval.PValue, bool, error) {
-		v, ok := sampleData[key]
-		return v, ok, nil
-	}
-
-	lookup.DoWithParent(context.Background(), provider, func(c eval.Context) error {
-		v := c.Evaluate(c.ParseAndValidate(`sample.pp`, `lookup('first')`, false))
+	err := lookup.DoWithParent(context.Background(), sampleProvider, func(c eval.Context) error {
+		v, _ := lookup.Lookup(c, []string{`first`}, nil, nil)
 		fmt.Println(v)
-		v = c.Evaluate(c.ParseAndValidate(`sample.pp`, `lookup('arr.1')`, false))
+		return nil
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+	// Output:
+	// value of first
+}
+
+func ExampleLookup_dottedInt() {
+	lookup.DoWithParent(context.Background(), sampleProvider, func(c eval.Context) error {
+		v, _ := lookup.Lookup(c, []string{`arr.1`}, nil, nil)
 		fmt.Println(v)
 		return nil
 	})
 	// Output:
-	// value of first
 	// two
+}
+
+func ExampleLookup_dottedStringInt() {
+	lookup.DoWithParent(context.Background(), sampleProvider, func(c eval.Context) error {
+		v, _ := lookup.Lookup(c, []string{`hash.arr.0`}, nil, nil)
+		fmt.Println(v)
+		return nil
+	})
+	// Output:
+	// value 1
 }
